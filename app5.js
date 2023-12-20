445a4feed3269eed68786bcefb7e234fcaa13b7b
kittyKissYou <ubiuka@yandex.ru>
2023-12-20 14:34:19 +0900

    go

--- app5.js
+++ app5.js
@@ -17,52 +17,46 @@ class Config {
 }
 
 let report = {};
+/*
+const numberKeyboard = {
+  reply_markup: JSON.stringify({
+    inline_keyboard: [
+      [
+        { text: '1', callback_data: 1 },
+        { text: '2', callback_data: 2 },
+        { text: '3', callback_data: 3 },
+      ],
+      [
+        { text: '4', callback_data: 4 },
+        { text: '5', callback_data: 5 },
+        { text: '6', callback_data: 6 },
+      ],
+      [
+        { text: '7', callback_data: 7 },
+        { text: '8', callback_data: 8 },
+        { text: '9', callback_data: 9 },
+      ],
+      [{ text: '0', callback_data: 0 }],
+    ],
+  }),
+};
+*/
+const booleanKeyboard = {
+  reply_markup: JSON.stringify({
+    inline_keyboard: [
+      [{ text: 'Все верно', callback_data: 'true' }],
+      [{ text: 'Изменить', callback_data: 'false' }],
+    ],
+  }),
+};
 
-bot.on('message', async (msg) => {
-  const chat = msg.chat.id;
-  const user = msg.from.id;
-  const message = msg.text;
-
+const reportFunction = (chat, user, message, userName, tgName) => {
   if (message === '/start') {
     bot.sendMessage(
       user,
-      `Привет, ${msg.from.first_name}!\nНе отвечай на это сообщение!\nЯ напишу тебе сам, после того как ты напишешь вечерний отчет.`
-    );
-    console.log(
-      `${msg.from.first_name + ' ' + msg.from.username} is avalible now`
+      `Привет, ${userName}!\nНе отвечай на это сообщение!\nЯ напишу тебе сам, после того как ты напишешь вечерний отчет.`
     );
   } else {
-    /*
-    let config = {
-      method: 'get',
-      maxBodyLength: Infinity,
-      url: `https://script.google.com/macros/s/AKfycbxzasy7td4YGLjMU3o-q5UrmFG3JtOdiJw5lBKcxCzYoulgBB4Acr_OGmtOwIisIoDTKg/exec?data=${
-        msg.from.first_name + ' ' + msg.from.username
-      }`,
-      headers: {},
-    };
-    axios
-      .request(config)
-      .then((response) => {
-        if (!response.data) {
-        } else {
-          bot.sendMessage(user, `Сегодня вы уже давали отчет`);
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    /*
-  setInterval(() => {
-    //.add(6, 'hours')
-    if (moment().format('HH:mm') === '17:52') {
-      person = false;
-      console.log('Можно давать отчет');
-    } else {
-      console.log('пока рано');
-    }
-  }, 60000);
-  */
     if (
       chat === -1001821085070 || // вечерняя
       chat === -4071454549 || // тест3
@@ -86,23 +80,22 @@ bot.on('message', async (msg) => {
         user === 6368983749 || // Бондаренко
         user === 5805007839 // Иванова Нина
       ) {
-        bot.sendMessage(user, `Привет, ${msg.from.first_name}!`);
-        bot.sendMessage(user, 'Напиши сюда сколько сегодня было БС');
+        bot.sendMessage(user, `Привет, ${userName}!`);
+        bot.sendMessage(user, 'Cколько сегодня было БС');
       }
     }
 
     if (chat === user) {
-      if (msg.text / 0 === Infinity || msg.text == 0) {
+      if (message / 0 === Infinity || message == 0) {
         if (!report.svk && !report.bs) {
-          report.svk = msg.from.first_name + ' ' + msg.from.username;
-
-          report.bs = Number(msg.text);
+          report.svk = userName + ' ' + tgName;
+          report.bs = Number(message);
           return bot.sendMessage(user, `Сколько было фондирований`);
         }
 
         if (report.funding === undefined) {
-          if (Number(msg.text) <= report.bs) {
-            report.funding = Number(msg.text);
+          if (Number(message) <= report.bs) {
+            report.funding = Number(message);
             return bot.sendMessage(
               user,
               `Сколько всего было предложений по Кросс КК или Комбо`
@@ -115,12 +108,12 @@ bot.on('message', async (msg) => {
           }
         }
         if (report.offerKK === undefined) {
-          report.offerKK = Number(msg.text);
+          report.offerKK = Number(message);
           return bot.sendMessage(user, 'Сколько из них было фактически выдано');
         }
         if (report.crossKK === undefined) {
-          if (Number(msg.text) <= report.offerKK) {
-            report.crossKK = Number(msg.text);
+          if (Number(message) <= report.offerKK) {
+            report.crossKK = Number(message);
             report.refusalOfferKK = report.offerKK - report.crossKK;
             return bot.sendMessage(user, `Сколько было кросс ДК`);
           } else {
@@ -132,28 +125,28 @@ bot.on('message', async (msg) => {
         }
 
         if (report.crossDK === undefined) {
-          report.crossDK = Number(msg.text);
+          report.crossDK = Number(message);
           return bot.sendMessage(user, 'Сколько было всего выдано Селфи ДК');
         }
         if (report.selfieDK === undefined) {
-          report.selfieDK = Number(msg.text);
+          report.selfieDK = Number(message);
           return bot.sendMessage(user, 'Сколько было всего выдано Селфи КК');
         }
 
         if (report.selfieKK === undefined) {
-          report.selfieKK = Number(msg.text);
+          report.selfieKK = Number(message);
           return bot.sendMessage(user, 'Сколько всего было клиентов с айфоном');
         }
         if (report.iphone === undefined) {
-          report.iphone = Number(msg.text);
+          report.iphone = Number(message);
           return bot.sendMessage(
             user,
             'Сколько было установлено приложений на айфон'
           );
         }
         if (report.ios === undefined) {
-          if (Number(msg.text) <= report.iphone) {
-            report.ios = Number(msg.text);
+          if (Number(message) <= report.iphone) {
+            report.ios = Number(message);
             return bot.sendMessage(user, 'Сколько было всего сделано ЦП');
           } else {
             return bot.sendMessage(
@@ -164,33 +157,56 @@ bot.on('message', async (msg) => {
         }
 
         if (report.cp === undefined) {
-          report.cp = Number(msg.text);
+          report.cp = Number(message);
           report.date = moment().add(6, 'hours').format('DD.MM.YYYY HH:mm:ss');
-          let bonus =
-            report.funding * 600 +
-            report.crossKK * 470 +
-            report.crossDK * 300 +
-            report.selfieDK * 300 +
-            report.selfieKK * 470 +
-            report.cp * 100;
+
           bot.sendMessage(
             user,
-            `Спасибо за отчет!\nПо моим подсчетам, твоя премия за кросс-продукты составляет ${bonus} руб.`
+            `Подтвердите данные перед отправкой\nБС ${report.bs}\nФондирований ${report.funding}\nПредложений по кросс КК ${report.offerKK}\nКросс КК ${report.crossKK}\nКросс ДК ${report.crossDK}\nСелфи ДК ${report.selfieDK}\nСелфи КК ${report.selfieKK}\nАйфонов ${report.iphone}\nУстановок приложения ${report.ios}\nЦП ${report.cp}`,
+            booleanKeyboard
           );
-          const googleData = new Config(report);
-          axios
-            .request(googleData)
-            .then((response) => {
-              console.log(JSON.stringify(response.data));
-              report = {};
-            })
-            .catch((error) => {
-              console.log(error);
-            });
         }
       } else {
         bot.sendMessage(user, 'Ответ должен содержать числовое значение');
       }
     }
+  }
+};
+bot.on('message', async (msg) => {
+  const chat = msg.chat.id;
+  const user = msg.from.id;
+  const message = msg.text;
+  const userName = msg.from.first_name;
+  const tgName = msg.from.username;
+  reportFunction(chat, user, message, userName, tgName);
+});
+bot.on('callback_query', (msg) => {
+  const user = msg.from.id;
+  let data = msg.data === 'true' ? true : false;
+  if (data) {
+    const googleData = new Config(report);
+    let bonus =
+      report.funding * 600 +
+      report.crossKK * 470 +
+      report.crossDK * 300 +
+      report.selfieDK * 300 +
+      report.selfieKK * 470 +
+      report.cp * 100;
+    report = {};
+    axios
+      .request(googleData)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+        return bot.sendMessage(
+          user,
+          `Спасибо за отчет!\nПо моим подсчетам, твоя премия за кросс-продукты составляет ${bonus} руб.`
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } else {
+    report = {};
+    bot.sendMessage(user, 'Сколько было БС');
   }
 });
