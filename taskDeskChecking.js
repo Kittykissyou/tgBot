1bec3d208177db2a18af23a59c651874a09d290c
Kittykissyou <ubiuka@yandex.ru>
2025-04-06 18:33:33 +0900

    go

--- taskDeskChecking.js
+++ taskDeskChecking.js
@@ -9,33 +9,34 @@ let config = {
   url: 'https://script.google.com/macros/s/AKfycbyfaEr64EOb1gxJKBGyExpeDIrGxgsgEXq7P-YIUeaL7Ftfq23TuMVcAGyxqnphDAlNPQ/exec',
   headers: {},
 };
-
-axios
-  .request(config)
-  .then((response) => {
-    if (response.data.length != 0) {
-      let report = response.data;
-      for (let i = 0; i <= response.data.length - 1; i++) {
-        bot.sendMessage(
-          847331105,
-          `*${moment()
-            .add(6, 'hours')
-            .format('DD.MM.YYYY HH:mm')}*\n\n*Новая задача!*\n${
-            report[i].task
-          }\n*Дата постановки:* ${moment(report[i].dateStart).format(
-            'DD.MM.YYYY'
-          )}\n*Дата завершения:* ${moment(report[i].dateFinish).format(
-            'DD.MM.YYYY'
-          )}\n${
-            report[i].description != ''
-              ? `*Комментарий:* ${report[i].description}`
-              : ``
-          }\n${report[i].person === 'Все' ? `*Для всех.*` : ''}`,
-          { parse_mode: 'Markdown' }
-        );
+setInterval(() => {
+  axios
+    .request(config)
+    .then((response) => {
+      if (response.data.length != 0) {
+        let report = response.data;
+        for (let i = 0; i <= response.data.length - 1; i++) {
+          bot.sendMessage(
+            847331105,
+            `*${moment()
+              .add(6, 'hours')
+              .format('DD.MM.YYYY HH:mm')}*\n\n*Новая задача!*\n${
+              report[i].task
+            }\n*Дата постановки:* ${moment(report[i].dateStart).format(
+              'DD.MM.YYYY'
+            )}\n*Дата завершения:* ${moment(report[i].dateFinish).format(
+              'DD.MM.YYYY'
+            )}\n${
+              report[i].description != ''
+                ? `*Комментарий:* ${report[i].description}`
+                : ``
+            }\n${report[i].person === 'Все' ? `*Для всех.*` : ''}`,
+            { parse_mode: 'Markdown' }
+          );
+        }
       }
-    }
-  })
-  .catch((error) => {
-    bot.sendMessage(847331105, `У нас ошибка:\n${error}`);
-  });
+    })
+    .catch((error) => {
+      bot.sendMessage(847331105, `У нас ошибка:\n${error}`);
+    });
+}, 3600000);
